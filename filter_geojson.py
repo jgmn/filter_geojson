cb8bb071fec9f44512128d5f64dd412e76757d7e
jgmn <jesus.gerardo.mn@outlook.com>
2017-06-22 15:50:35 +0200

    Calculo de centroides de poligonos

--- filter_geojson.py
+++ filter_geojson.py
@@ -1,6 +1,7 @@
 import json
 import calendar
 from datetime import datetime
+from shapely.geometry import Polygon
 
 # Path where the input file is located
 path_input_file = 'calificaciones.JSON'
@@ -11,30 +12,32 @@ print ("Reading input file...")
 with open(path_input_file,"r") as input_file:
     data = json.load(input_file)
 
-# Filter JSON data
+# Filter JSON data and calculating Polygons centroid
 print("Filtering JSON data...")
-
+print("Calculating Polygons centroid...")
 
 result = {}
 result['type'] = data['type']
 result['crs'] = data['crs']
 result['features'] = []
 
+temp = []
+
 for feature in data['features']:
-    if feature['properties']['califi'] == "EDA" and feature['properties']['uso'] == "SJL":
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "PID":
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "GTR" and feature['properties']['tipoca'] == "4":
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "GSP" and (feature['properties']['tipoca'] == "4" or feature['properties']['tipoca'] == "3"):
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "TER" and feature['properties']['tipoca'] == "3":
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "CHP" and feature['properties']['tipoca'] == "134":
-        result['features'].append(feature)
-    elif feature['properties']['califi'] == "E/SP" and feature['properties']['tipoca'] == "1P":
-        result['features'].append(feature)
+    if ((feature['properties']['califi'] == "EDA" and  feature['properties']['uso'] == "SJL")
+        or (feature['properties']['califi'] == "PID")
+        or (feature['properties']['califi'] == "GTR"  and  feature['properties']['tipoca'] == "4")
+        or (feature['properties']['califi'] == "GSP"  and (feature['properties']['tipoca'] == "4" or feature['properties']['tipoca'] == "3"))
+        or (feature['properties']['califi'] == "TER"  and  feature['properties']['tipoca'] == "3")
+        or (feature['properties']['califi'] == "CHP"  and  feature['properties']['tipoca'] == "134")
+        or (feature['properties']['califi'] == "E/SP" and  feature['properties']['tipoca'] == "1P")):
+        feature['geometry']['type'] = "Point"
+        for coordinate in feature['geometry']['coordinates'][0]:
+            temp.append(tuple(coordinate))
+        polygon = Polygon(temp)
+        point = polygon.envelope.centroid
+        feature['geometry']['coordinates'] = [point.x, point.y]
+        result['features'].append(feature) 
 
 # Write output file
 print('Writting output file...')
