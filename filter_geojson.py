aa155287ff0c0ee48aa6a3e4f12026f37eaa03ab
jgmn <jesus.gerardo.mn@outlook.com>
2017-06-22 11:46:35 +0200

    Cambio nombre de variable

--- filter_geojson.py
+++ filter_geojson.py
@@ -1,5 +1,4 @@
 import json
-
 import calendar
 from datetime import datetime
 
@@ -15,26 +14,27 @@ with open(path_input_file,"r") as input_file:
 # Filter JSON data
 print("Filtering JSON data...")
 
-temp = {}
-temp['type'] = data['type']
-temp['crs'] = data['crs']
-temp['features'] = []
+
+result = {}
+result['type'] = data['type']
+result['crs'] = data['crs']
+result['features'] = []
 
 for feature in data['features']:
     if feature['properties']['califi'] == "EDA" and feature['properties']['uso'] == "SJL":
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "PID":
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "GTR" and feature['properties']['tipoca'] == "4":
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "GSP" and (feature['properties']['tipoca'] == "4" or feature['properties']['tipoca'] == "3"):
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "TER" and feature['properties']['tipoca'] == "3":
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "CHP" and feature['properties']['tipoca'] == "134":
-        temp['features'].append(feature)
+        result['features'].append(feature)
     elif feature['properties']['califi'] == "E/SP" and feature['properties']['tipoca'] == "1P":
-        temp['features'].append(feature)
+        result['features'].append(feature)
 
 # Write output file
 print('Writting output file...')
@@ -48,7 +48,7 @@ date = date.replace(" ", "")
 path_output_file = 'calificaciones_'+date+'.JSON'
 
 with open(path_output_file, "w") as output_file:
-    json.dump((temp), output_file, indent = 4)
+    json.dump((result), output_file, indent = 4)
 
 # Ready
 print("Ready")
